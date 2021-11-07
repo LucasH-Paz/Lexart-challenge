f14f991e90e47f0a97f1ce29658f7d62f1d01c6f
lucas-paz <lucas.hpaz@outlook.com>
2021-11-07 19:13:40 -0300

    styles to loading

--- src/Components/Loading/index.jsx
+++ src/Components/Loading/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './styles.css';
 
 const Loading = () => (
-  <div>
-    Loading
+  <div className="loading">
+    <div className="lds-ellipsis">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
   </div>
 );
 
